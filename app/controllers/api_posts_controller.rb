fb0b7e3ecbe39fb3750e30ac531ac118a1650a26
Ryuya-web <ryuya00617@gmail.com>
2021-07-15 16:12:17 +0900

    api追加

--- app/controllers/api_posts_controller.rb
+++ app/controllers/api_posts_controller.rb
@@ -1,7 +1,12 @@
 class ApiPostsController < ApplicationController
     def index
+        @posts = Post.order(created_at: :desc)
+        render json: { status: 'SUCCESS', message: 'Loaded posts', data: @posts }
     end
-
+    
+    # GET /posts/1 or /posts/1.json
     def show
+    @post = Post.find(params[:id])
+    render json: { status: 'SUCCESS', message: 'Loaded the post', data: @post }
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   root 'posts#index'
-  namespace :api, format: 'json' do
-    get 'posts/preview'
-  end
+  resources :api_posts, only: [:index, :show] 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
